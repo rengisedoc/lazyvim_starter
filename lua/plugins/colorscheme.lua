b036af6d069e485afe91c21b041e6029c70ec442
Ricky Hsia <3862182+rengisedoc@users.noreply.github.com>
2024-07-26 14:36:06 +0800

    feat: modify key mappings

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      transparent = true,
+      style = "night",
+    },
+  },
+}
---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separatos = {
+          left = "",
+          right = "",
+        },
+        section_separators = {
+          left = "",
+          right = "",
+        },
+      },
+    },
+  },
+}

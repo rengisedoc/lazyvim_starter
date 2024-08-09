36aed2b6939d1ad28089fda76c452a54e3eafc3e
Jiang Shang <jiangshang@tuya.com>
2024-08-09 10:13:38 +0800

    fix: add nvim-autopairs plugin instead of mini.pairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.ai",
+    enabled = false,
+  },
+}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,6 @@
+-- stylua: ignore
+if true then return {} end
+
 local icons = LazyVim.config.icons
 
 return {
